f2d3c75c91d08d1427f987871fac186d2cd9d218
xavialfaro <nxb.developer@gmail.com>
2022-09-09 13:35:47 -0500

    add configuration api

--- extension/src/constants.ts
+++ extension/src/constants.ts
@@ -1,6 +1,6 @@
 const __prod__ = process.env.NODE_ENV === 'production'
 
-export const API_BASE_URL = __prod__ ? 'PUT_URL_API' : 'http://localhost:3001'
+export const API_BASE_URL = __prod__ ? 'extensify-server.up.railway.app' : 'http://localhost:3001'
 
 export const GITHUB_CLIENT_ID = '66faaeafddf08f6bc17f'
 export const GITHUB_CLIENT_SECRET = '468a43a5991719ef4e7279c306232f8bcbbe83b8'
