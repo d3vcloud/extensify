109119041eec9aca81f1df58ba101218b7b75683
xavialfaro <nxb.developer@gmail.com>
2022-09-19 22:01:35 -0500

    remove extensify from user list extensions

--- extension/src/utils.ts
+++ extension/src/utils.ts
@@ -4,14 +4,16 @@ import { Extension } from './types/Extension'
 
 export const getExtensionList = (cb?: (message: string) => void): Extension[] | [] => {
   const myLocalExtensions = vscode.extensions.all.filter((ext) => !ext.packageJSON.isBuiltin)
+  // Remove my own extension from the list
+  const extensionsList = myLocalExtensions.filter((ext) => ext.id !== 'xavimon.extensify')
 
-  if (myLocalExtensions.length === 0) return []
+  if (extensionsList.length === 0) return []
 
   if (cb) {
     cb(`Extensions to upload:`)
     cb(`--------------------`)
   }
-  const extensions: Extension[] = myLocalExtensions.map((ext) => {
+  const extensions: Extension[] = extensionsList.map((ext) => {
     const { id, uuid, author, displayName, version, publisher, name } = ext.packageJSON
     const installationId = `${publisher}.${name}`
 
