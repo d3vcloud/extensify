290c8ed4a9d510f9eee1f10b8514bd683be75bc8
xavialfaro <nxb.developer@gmail.com>
2022-09-22 19:10:26 -0500

    update api URL

--- extension/src/constants.ts
+++ extension/src/constants.ts
@@ -1,8 +1,6 @@
 const __prod__ = process.env.NODE_ENV === 'production'
 
-export const API_BASE_URL = __prod__
-  ? 'https://extensify-server.up.railway.app'
-  : 'http://localhost:3001'
+export const API_BASE_URL = __prod__ ? 'https://api.vsextensify.com' : 'http://localhost:3001'
 
 export const GITHUB_CLIENT_ID = '66faaeafddf08f6bc17f'
 export const GITHUB_CLIENT_SECRET = '468a43a5991719ef4e7279c306232f8bcbbe83b8'
